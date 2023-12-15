64d557c3fb6a69d0170fdb251614c79ac93e7e4d
songtaehyeon <55786186+songtaehyeon@users.noreply.github.com>
2023-12-16 03:15:53 +0900

    Update onvif_send.py

--- code/onvif_send.py
+++ code/onvif_send.py
@@ -28,9 +28,8 @@ obe</w:Action>
 """
 socket.sendall(msg.encode(encoding='utf-8'))
 
-# 서버가 에코로 되돌려 보낸 메시지를 클라이언트가 받음
 data = socket.recv(100)
-msg = data.decode() # 읽은 데이터 디코딩
+msg = data.decode() 
 print('echo msg:', msg)
 
 socket.close()
