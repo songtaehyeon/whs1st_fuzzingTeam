ce1f65d373b82fa00d37928c4a01016839362bed
unknown <songtaehyeon>
2023-12-16 03:13:15 +0900

    .

---
+++ code/onvif_send.py
@@ -0,0 +1,36 @@
+import socket
+
+target_ip = "192.168.47.130"
+target_port = 34567
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((target_ip,target_port))
+
+msg = """<?xml version="1.0" encoding="UTF-8"?>
+<e:Envelope xmlns:e="http://www.w3.org/2003/05/soap-envelope"
+ xmlns:w="http://schemas.xmlsoap.org/ws/2004/08/addressing"
+ xmlns:d="http://schemas.xmlsoap.org/ws/2005/04/discovery"
+ xmlns:dn="http://www.onvif.org/ver10/network/wsdl">
+ <e:Header>
+ <w:MessageID>uuid:84ede3de-7dec-11d0-c360-f01234567890</w:MessageID>
+ <w:To e:mustUnderstand="true">urn:schemas-xmlsoap-org:ws:2005:04:discovery</w:To>
+ <w:Action
+a:mustUnderstand="true">http://schemas.xmlsoap.org/ws/2005/04/discovery/Pr
+obe</w:Action>
+ </e:Header>
+ <e:Body>
+ <d:Probe>
+ <d:Types>dn:NetworkVideoTransmitter</d:Types>
+ </d:Probe>
+ </e:Body>
+</e:Envelope>
+
+"""
+socket.sendall(msg.encode(encoding='utf-8'))
+
+# 서버가 에코로 되돌려 보낸 메시지를 클라이언트가 받음
+data = socket.recv(100)
+msg = data.decode() # 읽은 데이터 디코딩
+print('echo msg:', msg)
+
+socket.close()
