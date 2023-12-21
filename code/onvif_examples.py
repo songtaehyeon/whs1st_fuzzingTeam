b7e8b4d37cb4d0c231559d1dfc9044f9dbe08317
songtaehyeon <abesse0109@naver.com>
2023-12-21 20:16:00 +0900

    .

--- code/onvif_examples.py
+++ code/onvif_examples.py
@@ -2,26 +2,25 @@ import os
 import random
 import string
 
-def data_change(input_data):
-	for i in range(10):
-		input_data.replace("?",''.join(random.choice(string.ascii_letters + string.digits + string.punctuation + string.whitespace)for j in range(random.randint(5, 50))))
-	print(input_data)
+def data_change(input_line):
+    output_line = ""
+    i = 0
+    while i < len(input_line):
+        if input_line[i] == "?":
+            random_str = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(random.randint(5, 50)))
+            output_line += random_str
+            i += 1
+        else:
+            output_line += input_line[i]
+            i += 1
+    return output_line
 
-def read_data(data):
-	for file_line in range(len(data)):
-		if "?" in data[file_line]:
-			data_change(data[file_line])
-
-data_folder = "../extract_data/split"
-file_list = os.listdir(data_folder)
-
-for file_num in range(1):
-	files = file_list[file_num] if file_list else None
-
-	if files:
-		file_path = os.path.join(data_folder,files)
-		with open(file_path) as file:
-			data = file.readlines()
-			read_data(data)
-	else:
-		print("NOPE")
+def process_file(file_path):
+    with open(file_path, 'r') as file:
+        file_data = file.readlines() #파일안의 내용이 리스트로 나옴
+        for file_line in range(len(file_data)): # 라인 별로 체크
+            line = file_data[file_line] #file_line 번째 줄이 나옴
+            file_data[file_line] = data_change(line) #?체크
+        combine_filedata = ''.join(file_data)
+        print(combine_filedata)
+        yield combine_filedata
--- code/onvif_send.py
+++ code/onvif_send.py
@@ -1,17 +1,32 @@
 import socket
-import onvif_exaples.py
-
+from onvif_examples import process_file
+import random
+import os
+import string
+import time
 
 target_ip = "192.168.47.130"
-target_port = 34567
+target_port = 8899
+
+random.seed(1234)
+
+cnt = 0
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((target_ip,target_port))
 
-socket.sendall(msg.encode(encoding='utf-8'))
+data_folder = "../extract_data/split"
+file_list = os.listdir(data_folder)
+while 1:
+	for file_name in file_list[0:]: 
+		file_path = os.path.join(data_folder, file_name)
+		print(file_path)
+		packets = process_file(file_path)
+		for pkt in packets:
+			socket.sendall(pkt.encode(encoding='utf-8'))
+			time.sleep(0.1)
+			print(cnt)
+			cnt += 1
 
-data = socket.recv(100)
-msg = data.decode() 
-print('echo msg:', msg)
 
 socket.close()
